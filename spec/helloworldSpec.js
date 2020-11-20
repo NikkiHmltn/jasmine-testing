d0dae6d8faaf60d13b2dbc42bae7adcfbc7a38ee
Nicole Hamilton <Nicole@NicolesLaptop.local>
2020-11-20 14:44:24 -0800

    [app] built a test in jasmine

---
+++ spec/helloworldSpec.js
@@ -0,0 +1,7 @@
+describe('Hello World', function() {
+    // const printThis = require('../lib/helloworld')
+    it('should return hello world', function() {
+        let result = printThis();
+        expect(result).toEqual('Hello World')
+    })
+})
